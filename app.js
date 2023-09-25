0ee4474c8354fa6f95aa08d9b8b347d0986002ad
LUKE REYNOLDS <luke.reynolds.92@gmail.com>
2023-09-25 19:40:56 +0100

    update js with each location

--- app.js
+++ app.js
@@ -1,23 +1,7 @@
-// Within your javascript file, create separate JS object literals for each shop location. Each location will be responsible for generating sales data and providing output on an html document.
-
-// Pat will need to be able to add and remove locations from the daily projections report, and Pat will also need to be able to easily modify the input numbers for each location based on day of the week, special events, and other factors. Pat would like to see these numbers with nice formatting in a web application.
-
-// So, in addition to building an application that calculates daily sales projections for each location (on a page called sales.html), you also need to create a public-facing page (on the homepage index.html) that is colorful, eye-catching, readable, useful, and informative.
-
-//const trevor = {
-// name: "Trevor",
-//  age: 2,
-// interests: ["Philosophy", "Bacon", "Fish", "Minimilism"],
-// image: "./images/trevor.jpg",
-// goodWithKids: true,
-// goodWithDogs: false,
-// goodWithCats: false,
-// breed: "ginger",
-// };
-
-// create our first shop
-
-console.log("hello world");
+// Use a method of that object to generate a random number of customers per hour. Objects/Math/random{:target="_blank"}
+function randomNumber(min, max) {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
 
 const hours = [
   "6am",
@@ -36,12 +20,7 @@ const hours = [
   "7pm",
 ];
 
-// give a random number between two numbers
-function randomNumber(min, max) {
-  return Math.floor(Math.random() * (max - min + 1)) + min;
-}
-
-// create our first shop
+// Store the min/max hourly customers, and the average cookies per customer, in object properties.
 const seattle = {
   location: "seattle",
   minCust: 23,
@@ -51,14 +30,85 @@ const seattle = {
   cookiesPerHour: [],
   totalCookieSold: 0,
   calculateSales: function () {
-    // add a random number to the customersPerHour array
+    for (let i = 0; i < hours.length; i++) {
+      // add a random number to the customersPerHour array
+      const randNum = randomNumber(this.minCust, this.maxCust);
+      this.customersPerHour.push(randNum);
+      this.cookiesPerHour.push(randNum * this.avgCookiesPerCust);
+    } //Calculate and store the simulated amounts of cookies purchased for each hour at each location using average cookies purchased and the random number of customers generated.
+  },
+};
+
+const tokyo = {
+  location: "tokyo",
+  minCust: 3,
+  maxCust: 24,
+  avgCookiesPerCust: 1.2,
+  customersPerHour: [],
+  cookiesPerHour: [],
+  totalCookiesSold: 0,
+  calculateSales: function () {
     for (let i = 0; i < hours.length; i++) {
       const randNum = randomNumber(this.minCust, this.maxCust);
       this.customersPerHour.push(randNum);
       this.cookiesPerHour.push(randNum * this.avgCookiesPerCust);
+      this.totalCookiesSold += this.cookiesPerHour[i];
     }
   },
 };
 
-seattle.calculateSales();
-console.log(seattle);
+const dubai = {
+  location: "dubai",
+  minCust: 11,
+  maxCust: 38,
+  avgCookiesPerCust: 3.7,
+  customersPerHour: [],
+  cookiesPerHour: [],
+  totalCookiesSold: 0,
+  calculateSales: function () {
+    for (let i = 0; i < hours.length; i++) {
+      const randNum = randomNumber(this.minCust, this.maxCust);
+      this.customersPerHour.push(randNum);
+      this.cookiesPerHour.push(randNum * this.avgCookiesPerCust);
+      this.totalCookiesSold += this.cookiesPerHour[i];
+    }
+  },
+};
+
+const paris = {
+  location: "paris",
+  minCust: 20,
+  maxCust: 38,
+  avgCookiesPerCust: 2.3,
+  customersPerHour: [],
+  cookiesPerHour: [],
+  totalCookiesSold: 0,
+  calculateSales: function () {
+    for (let i = 0; i < hours.length; i++) {
+      const randNum = randomNumber(this.minCust, this.maxCust);
+      this.customersPerHour.push(randNum);
+      this.cookiesPerHour.push(randNum * this.avgCookiesPerCust);
+      this.totalCookiesSold += this.cookiesPerHour[i];
+    }
+  },
+};
+
+const lima = {
+  location: "lima",
+  minCust: 2,
+  maxCust: 16,
+  avgCookiesPerCust: 4.6,
+  customersPerHour: [],
+  cookiesPerHour: [],
+  totalCookiesSold: 0,
+  calculateSales: function () {
+    for (let i = 0; i < hours.length; i++) {
+      const randNum = randomNumber(this.minCust, this.maxCust);
+      this.customersPerHour.push(randNum);
+      this.cookiesPerHour.push(randNum * this.avgCookiesPerCust);
+      this.totalCookiesSold += this.cookiesPerHour[i];
+    }
+  },
+};
+
+const locations = [seattle, tokyo, dubai, paris, lima]; // Store the results for each location in a separate array... perhaps as a property of the object representing that location.
