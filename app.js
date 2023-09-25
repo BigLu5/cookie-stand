9febd90f7e879e6edbfbad9cabf83ff93f2e9e63
LUKE REYNOLDS <luke.reynolds.92@gmail.com>
2023-09-25 15:47:28 +0100

    update js and html

--- app.js
+++ app.js
@@ -50,14 +50,15 @@ const seattle = {
   customersPerHour: [],
   cookiesPerHour: [],
   totalCookieSold: 0,
-  calcCustPerHour: function () {
+  calculateSales: function () {
     // add a random number to the customersPerHour array
     for (let i = 0; i < hours.length; i++) {
       const randNum = randomNumber(this.minCust, this.maxCust);
       this.customersPerHour.push(randNum);
+      this.cookiesPerHour.push(randNum * this.avgCookiesPerCust);
     }
   },
 };
 
-seattle.calcCustPerHour();
+seattle.calculateSales();
 console.log(seattle);
