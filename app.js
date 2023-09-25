7f5b2772900c8de9dd0d09385ac1c8804bcce6d4
LUKE REYNOLDS <luke.reynolds.92@gmail.com>
2023-09-25 15:22:12 +0100

    add html and js

---
+++ app.js
@@ -0,0 +1,63 @@
+// Within your javascript file, create separate JS object literals for each shop location. Each location will be responsible for generating sales data and providing output on an html document.
+
+// Pat will need to be able to add and remove locations from the daily projections report, and Pat will also need to be able to easily modify the input numbers for each location based on day of the week, special events, and other factors. Pat would like to see these numbers with nice formatting in a web application.
+
+// So, in addition to building an application that calculates daily sales projections for each location (on a page called sales.html), you also need to create a public-facing page (on the homepage index.html) that is colorful, eye-catching, readable, useful, and informative.
+
+//const trevor = {
+// name: "Trevor",
+//  age: 2,
+// interests: ["Philosophy", "Bacon", "Fish", "Minimilism"],
+// image: "./images/trevor.jpg",
+// goodWithKids: true,
+// goodWithDogs: false,
+// goodWithCats: false,
+// breed: "ginger",
+// };
+
+// create our first shop
+
+console.log("hello world");
+
+const hours = [
+  "6am",
+  "7am",
+  "8am",
+  "9am",
+  "10am",
+  "11am",
+  "12pm",
+  "1pm",
+  "2pm",
+  "3pm",
+  "4pm",
+  "5pm",
+  "6pm",
+  "7pm",
+];
+
+// give a random number between two numbers
+function randomNumber(min, max) {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+// create our first shop
+const seattle = {
+  location: "seattle",
+  minCust: 23,
+  maxCust: 65,
+  avgCookiesPerCust: 6.3,
+  customersPerHour: [],
+  cookiesPerHour: [],
+  totalCookieSold: 0,
+  calcCustPerHour: function () {
+    // add a random number to the customersPerHour array
+    for (let i = 0; i < hours.length; i++) {
+      const randNum = randomNumber(this.minCust, this.maxCust);
+      this.customersPerHour.push(randNum);
+    }
+  },
+};
+
+seattle.calcCustPerHour();
+console.log(seattle);
